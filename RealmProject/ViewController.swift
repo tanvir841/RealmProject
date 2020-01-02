95d706fab93403405426c5c34f0d2ef00a6492ee
Abdullah Al Numan <ab>
2020-01-01 20:00:17 -0800

    Initial Commit

---
+++ RealmProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmProject
+//
+//  Created by Tanvir on 1/1/20.
+//  Copyright © 2020 tanvir841. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
